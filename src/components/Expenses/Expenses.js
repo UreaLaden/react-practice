c7490586d9f4f93f9eca33edde9ae56b636d84aa
Leaundrae Mckinney <pytechdrae@gmail.com>
2021-06-05 00:01:26 -0400

    updated folder structure and functions

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import ExpenseItem from './ExpenseItem';
-import Card from './Card';
+import Card from '../UI/Card';
 import './Expenses.css';
 
-function Expenses(props) {
+const Expenses = (props) => {
   console.log(props.items);
   return (
     <Card className="expenses">
@@ -19,6 +19,6 @@ function Expenses(props) {
       })}
     </Card>
   );
-}
+};
 
 export default Expenses;
